b883a6edaf8a6c2af4f898f36d29ad45c2f314be
evadamur <133592560+evadamur@users.noreply.github.com>
2025-05-09 22:04:18 +0200

    Update Card.js

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-export function Card({ children, className = "" }) {
+export function Card({ children, className }) {
   return (
-    <div className={`p-6 rounded-lg shadow-lg bg-gray-800 ${className}`}>
+    <div className={`p-6 rounded-xl shadow-lg bg-white ${className}`}>
       {children}
     </div>
   );
 }
 
 export function CardContent({ children }) {
-  return <div className="p-4">{children}</div>;
+  return <div className="text-gray-800">{children}</div>;
 }
