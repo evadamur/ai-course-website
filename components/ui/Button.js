d146f7b850be64953668535e84c6c52e3c6bfd14
evadamur <133592560+evadamur@users.noreply.github.com>
2025-05-09 22:04:44 +0200

    Update Button.js

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export function Button({ children, className = "", ...props }) {
+export function Button({ children, className, ...props }) {
   return (
     <button
-      className={`px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-all duration-300 shadow-lg ${className}`}
+      className={`py-2 px-4 rounded-xl bg-blue-600 text-white hover:bg-blue-700 transition duration-300 ${className}`}
       {...props}
     >
       {children}
